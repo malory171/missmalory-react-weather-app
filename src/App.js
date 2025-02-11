ecaa847240e6b8abd5ddc0d3425acfe269fe0d5d
malory171 <malory171@gmail.com>
2025-02-11 16:04:36 +0100

    Added GitHub repo URL & added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>London</h1>
+      <div className="container">
+        <h1>London</h1>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/135692-malory-mezierre"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Malory Mézierre
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/malory171/missmalory-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
