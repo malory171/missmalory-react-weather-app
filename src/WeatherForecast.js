6c7971a284ea41ea1c260585b4a6f56567d7abaa
malory171 <malory171@gmail.com>
2025-02-25 19:42:51 +0100

    Forecast mapping to render the first 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,9 +23,17 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col p-0" key="index">
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
